98782821a898c91f4410bab8dec0dca6d8f128ce
ImeneeSh <nourelimene.sahi@se.univ-bejaia.dz>
2025-11-11 21:49:58 +0100

    première navigation établit

--- src/main/java/com/example/controllers/AuthentificationController.java
+++ src/main/java/com/example/controllers/AuthentificationController.java
@@ -1,6 +1,9 @@
 package com.example.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
@@ -8,6 +11,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AuthentificationController {
 
@@ -42,12 +47,36 @@ public class AuthentificationController {
         String email = emailField.getText();
         String password = passwordField.getText();
 
-        // TODO: Ajouter la logique d'authentification
         System.out.println("Email: " + email + ", Mot de passe: " + password);
+
+        try {
+            // Charger le FXML du tableau de bord
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/views/TableauBord.fxml"));
+            Parent root = loader.load();
+
+            // Créer la scène
+            Scene scene = new Scene(root);
+
+            // 🔹 Charger et appliquer les fichiers CSS
+            String tableauCss = getClass().getResource("/styles/tableauBord.css").toExternalForm();
+            String menuCss = getClass().getResource("/styles/menu.css").toExternalForm();
+            scene.getStylesheets().addAll(tableauCss, menuCss);
+
+            // Changer la scène dans la fenêtre actuelle
+            Stage stage = (Stage) btnLogin.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setTitle("Tableau de bord");
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
+
+
     private void ouvrirInscription() {
-        // TODO: Ouvrir la page d'inscription
+        // TODO: Redirection vers Inscription.fxml (plus tard)
         System.out.println("Redirection vers l'inscription...");
     }
 }
