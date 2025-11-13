3bfb838924da6cbcd0b23a7ed65a6750f1b0e940
linaMCH <maouchelina458@gmail>
2025-11-13 14:38:17 +0100

    test

---
+++ src/main/java/com/example/bdd/test.java
@@ -0,0 +1,22 @@
+package com.example.bdd;
+
+import java.sql.*;
+
+public class test {
+    public static void main(String[] args) {
+        try (Connection conn = DatabaseConnection.getConnection()) {
+            if (conn != null) {
+                String query = "SELECT * FROM client";
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+
+                while (rs.next()) {
+                    System.out.println(rs.getInt("codeClt") + " - " +
+                            rs.getString("prenom") + " - " );
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
